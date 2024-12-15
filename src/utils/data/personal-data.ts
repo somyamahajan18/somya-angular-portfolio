5b59600e660d79c643adfab009c66688b4388be0
Javeed Ishaq <ishaqjaveed1@gmail.com>
2024-12-15 14:42:47 +0500

    about section updated

--- src/utils/data/personal-data.ts
+++ src/utils/data/personal-data.ts
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Javeed Ishaq",
   profile: 'assets/profile.png',
   designation: "Software Developer",
-  description: "I am a professional and enthusiastic programmer with a passion for mobile and web development. With expertise in Flutter, React.js, and various other technologies, I bring ideas to life through clean, efficient code. I am a quick learner with a self-learning attitude, always eager to explore new technologies and solve complex problems. I specialize in building cross-platform mobile applications and responsive web interfaces that deliver exceptional user experiences.",
+  description: "I’m Javeed Ishaq, a professional mobile and web app developer with over 6.5 years of experience, specializing in crafting seamless, responsive, and efficient applications. My expertise spans Flutter, Android, and Angular, enabling me to develop cross-platform mobile apps and dynamic web interfaces that deliver exceptional user experiences.\n\nWith a strong foundation in Spring Boot, PostgreSQL, and other backend technologies, I have a holistic approach to building scalable, end-to-end solutions. My passion for innovation drives me to constantly learn and master new technologies, including Kotlin Multiplatform, as I strive to stay ahead in the ever-evolving tech landscape.\n\nFrom working on NFT utilities and custom vehicle rental systems to leading development teams, I’ve gained a deep understanding of the challenges and possibilities in modern app development. I’m a self-motivated problem solver who thrives on bringing ideas to life through clean, efficient, and maintainable code.\n\nLet’s build something amazing together!",
   email: 'ishaqjaveed1@gmail.com',
   phone: '+923217281104',
   address: 'Sahiwal, Punjab, Pakistan',
